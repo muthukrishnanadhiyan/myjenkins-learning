a49f2e59d31edafe8e6407b8665a135fdc1ac7da
mutukrishnan <muthukrishnang.careers@gmail.com>
2024-11-20 22:54:52 +0530

    new-changes

--- script.sh
+++ script.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-aws --version
-
+aws --version &> /dev/null/
+ 
 if [ $? -eq  0 ]; then
 
 echo " aws cli is installed "
 
-else 
+users=("muthu" "harish" "indhumathi")
 
-echo " Need to install awscli on ubnutu"
+for user in "${users[@]}"; do
 
+if id "$user" &> /dev/null; then
+
+echo " user already exists"
+
+else
+   echo " need to create a user"
+   useradd -m "$user" -s /bin/bash
+
+   fi
+   done
+
+   else
+     echo "AWS CLI is not installed. Installing AWS CLI on Ubuntu"
+     sudo apt-get update
+  sudo apt-get install -y awscli
+  
+  echo "AWS CLI installed successfully."
 fi
